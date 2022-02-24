a2fec680ea7ca51c734eff16d035d45445b8b98b
bhoomitpatel147 <90571835+bhoomitpatel147@users.noreply.github.com>
2022-02-24 14:19:45 -0500

    Add project files.

---
+++ MvcVase/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcVase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
